d9bd61fdf081c5ce1ff56b1f473e961cfd113f52
Braindie <zhangwenjun0630@163.com>
2019-03-27 11:15:22 +0800

    多态概念

--- BJOC/Class/polymorphism/BJPolymorphismViewController.m
+++ BJOC/Class/polymorphism/BJPolymorphismViewController.m
@@ -33,15 +33,13 @@
     
     Person *person = [[Person alloc] init];
     
-    ColorPrinter *colorPrint = [[ColorPrinter alloc] init];
-    BlackPrinter *blackPrint = [[BlackPrinter alloc] init];
+    Printer *colorPrint = [[ColorPrinter alloc] init];
+    Printer *blackPrint = [[BlackPrinter alloc] init];
     
-    BOOL isColorPrinter = NO;
-    if (isColorPrinter) {
-        [person doPrint:colorPrint];
-    }else{
-        [person doPrint:blackPrint];
-    }
+    //同一个方法作用于不同的类，产生不同的执行效果
+    //一般结合继承来使用，通过重写父类方法，来使得对同一类对象的同一方法的调用产生不同的结果
+    [person doPrint:colorPrint];
+    [person doPrint:blackPrint];
 }
 
 @end
