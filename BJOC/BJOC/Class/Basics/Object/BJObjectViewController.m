0c6cd9c568cc472d3699e577904ebd949010734f
zhangwenjun <zhangwenjun@xiaobu121.com>
2021-02-25 18:10:38 +0800

    对象内存分配和类型

--- BJOC/BJOC/Class/Basics/Object/BJObjectViewController.m
+++ BJOC/BJOC/Class/Basics/Object/BJObjectViewController.m
@@ -42,7 +42,6 @@ struct NSObject_IMPL {
 @end
 
 
-
 @implementation BJObjectViewController
 
 
@@ -51,13 +50,21 @@ struct NSObject_IMPL {
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     
+    [self object_type];
+}
+
+- (void)object_memory {
+    
 //    NSObject *obj = [[NSObject alloc] init];
     
     // NSObject实例对象的成员变量所占的大小 8
 //    NSLog(@"%zd", class_getInstanceSize([NSObject class]));
+    
+    // 分配内存对齐 Buckets
+    //#define NANO_MAX_SIZE            256 /* Buckets sized {16, 32, 48, ..., 256} */
+
     // obj指针所指向内存的大小 16
 //    NSLog(@"%zd", malloc_size((__bridge const void *)(obj)));
-    
     
     
     Student *stu = [[Student alloc] init];
@@ -65,18 +72,81 @@ struct NSObject_IMPL {
     stu.age = 2;
 //    stu.nom = 3;
     
+    
+//    NSLog(@"%zd", sizeof(int));
+//    NSLog(@"%zd", sizeof(NSInteger));
+//    NSLog(@"%zd", sizeof([NSArray class]));
+    
     NSLog(@"%zd", class_getInstanceSize([Student class]));
     NSLog(@"%zd", malloc_size((__bridge const void *)(stu)));
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)object_type {
+    
+    // 三种对象
+    
+    // instance对象，实例对象
+    NSObject *object1 = [[NSObject alloc] init];
+    NSObject *object2 = [[NSObject alloc] init];
+    
+    // class对象，类对象
+    Class objectClass1 = [object1 class];// class方法返回的一直是类对象
+    Class objectClass2 = [object2 class];
+    
+    Class objectClass3 = object_getClass(object1);
+    Class objectClass4 = object_getClass(object2);
+    
+    Class objectClass6 = objc_getClass([@"NSObject" UTF8String]);
+    
+    Class objectClass5 = [NSObject class];
+    
+    // meta-class对象，元类对象
+    Class objectMetaClass = object_getClass(objectClass5);// 将类对象当做参数，获得元类对象
+    
+    NSLog(@"instance - %p %p",
+          object1,
+          object2);
+    
+    NSLog(@"class - %p %p %p %p %p %p %d",
+          objectClass1,
+          objectClass2,
+          objectClass3,
+          objectClass4,
+          objectClass5,
+          objectClass6,
+          class_isMetaClass(objectClass3));
+    
+    NSLog(@"objectMetaClass - %p %d", objectMetaClass, class_isMetaClass(objectMetaClass));
+    
+    
+    /*
+     
+     1. Class objc_getClass(const char * _Nonnull name)
+        传入的是字符串类名，返回对应的类对象
+     
+     2. Class object_getClass(id _Nullable obj)
+        传入instance对象，返回class对象
+        传入class对象，返回mate-class对象
+        传入mate-class对象，返回NSObject（基类）的mate-class对象
+     
+     */
+    
+    
+    /*
+     
+     返回的都是类对象
+     
+     - (Class) {
+        return self->isa;
+     }
+     
+     + (Class) {
+        return self;
+     }
+     
+     */
+
 }
-*/
 
 @end
